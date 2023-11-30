43e673c872278b9161f3cdaffb13b3b41833baee
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-11-30 12:41:30 +0100

    test: first integration test

---
+++ tests/health_check.rs
@@ -0,0 +1,21 @@
+use zero2prod::run;
+
+fn spawn_app() {
+  let server = run().expect("Failed to bind address");
+  let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response= client
+        .get("http://127.0.0.1:8000/health-check")
+        .send()
+        .await
+        .expect("Failed to secure the request");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
