a95440827238f0de8dd5db9ac9b9af33e0401ac9
Alexandre COIN <alexandre@mbpdealexandre-1.home>
2023-12-01 05:11:51 +0100

    refactor to get random port for integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,22 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
-fn spawn_app() {
-  let server = run().expect("Failed to bind address");
-  let _ = tokio::spawn(server);
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
-    let response= client
-        .get("http://127.0.0.1:8000/health-check")
+    let response = client
+        .get(format!("{}/health-check", address))
         .send()
         .await
         .expect("Failed to secure the request");
